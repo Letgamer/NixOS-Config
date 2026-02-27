52f3f8556ac9d988ac3b04e157c5b14bee38cbf8
Letgamer <alexstephan005@gmail.com>
2026-02-27 21:07:20 +0100

    finalyzed hyprlock config

--- home-manager/modules/wayland.nix
+++ home-manager/modules/wayland.nix
@@ -81,7 +81,6 @@ in
   };
 
   # https://home-manager-options.extranix.com/?query=hyprlock&release=release-25.11
-  # TODO: test hyprlock config, as it just blackscreens using Qemu
   programs.hyprlock = {
     enable = true;
 
@@ -97,40 +96,28 @@ in
         blur_passes = 2;
       };
 
+      # Time
+      label = {
+        monitor = "";
+        text = "$TIME";
+        font_size = 100;
+        font_family = "JetBrains Mono Extrabold";
+        position = "0, 20%";
+      };
+
       input-field = {
         monitor = "";
-        size = "250, 60";
-        outline_thickness = 2;
+        size = "11%, 4%";
 
         dots_size = 0.2;
         dots_spacing = 0.35;
-        dots_center = true;
 
         fade_on_empty = false;
-        rounding = -1;
 
-        placeholder_text = "<i><span foreground=\"#cdd6f4\">Input Password...</span></i>";
-
-        hide_input = false;
-
-        position = "0, -200";
-        halign = "center";
-        valign = "center";
+        placeholder_text = "Input Password...";
+        
+        position = "0, -10%";
       };
-
-      label = [
-        {
-          monitor = "";
-          text = "cmd[update:1000] echo \"$(date +\"%-I:%M\")\"";
-          color = "rgba(242, 243, 244, 0.75)";
-          font_size = 95;
-          font_family = "JetBrains Mono Extrabold";
-
-          position = "0, 200";
-          halign = "center";
-          valign = "center";
-        }
-      ];
     };
   };
 
